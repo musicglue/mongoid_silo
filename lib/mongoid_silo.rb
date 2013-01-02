aa4d93337136d7635f884cfed1f328784e061eb6
John <john@musicglue.com>
2013-01-02 17:33:06 +0000

    Fix require when not working from path

--- lib/mongoid_silo.rb
+++ lib/mongoid_silo.rb
@@ -1,8 +1,8 @@
 require "mongoid_silo/version"
 require 'mongoid_silo/railtie' if defined?(Rails)
 require 'mongoid/silo'
-require 'models/silo'
-require 'workers/mongoid_silo/update_silo_worker'
+require_relative '../app/models/silo'
+require_relative '../app/workers/mongoid_silo/update_silo_worker'
 
 module MongoidSilo
 end
