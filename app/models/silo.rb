db9a30e0d9b633e415d7136f9e3fda20f2fad8e9
John <john@musicglue.com>
2013-01-03 10:34:45 +0000

    Add scope and override to_json

--- app/models/silo.rb
+++ app/models/silo.rb
@@ -9,8 +9,17 @@ class Silo
   field :silo_type, type: String
   field :bag, type: Hash
 
-
   index item_class: 1
   index item_id: 1
   index silo_type: 1
+
+  def to_json
+    @json ||= Multijson.encode bag
+  end
+
+  class << self
+    def for_id_and_class_with_name id, class_name, silo_name
+      where(item_class: class_name, item_id: id, silo_type: silo_name).first
+    end
+  end
 end
