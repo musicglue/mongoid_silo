d83f757e122c43b5cf6d829a4f2f73177e07ae54
John <john@musicglue.com>
2013-01-04 15:19:24 +0000

    Removed/renamed/moved classes appropriately.

--- lib/mongoid_silo.rb
+++ lib/mongoid_silo.rb
@@ -1,5 +1,6 @@
 require "mongoid_silo/version"
 require 'mongoid_silo/railtie' if defined?(Rails)
+require 'mongoid_silo/grain_belt'
 require 'mongoid/silo'
 
 Dir['./app/**/*.rb'].each{ |file| require file }
