e959ed67739c7904d595c2885f77f2c03aae381c
John <john@musicglue.com>
2013-01-04 14:09:39 +0000

    Prune defunct classes

--- spec/factories/silo_factory.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :silo do
-    item_id     { Moped::BSON::ObjectId.new }
-    item_class  "ClassName"
-    silo_type   "default"
-    bag         Hash.new
-  end
-end
