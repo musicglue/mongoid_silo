439a50ec16107898f0db6f24fa782691717696f0
Tobias Svensson <tob@tobiassvensson.co.uk>
2013-05-21 14:40:05 +0100

    Add String#constantize refinement.

--- app/workers/mongoid_silo/update_silo_worker.rb
+++ app/workers/mongoid_silo/update_silo_worker.rb
@@ -1,26 +1,37 @@
 require 'sidekiq'
 
 module MongoidSilo
+  refine String do
+    def constantize
+      Object.module_eval(":#{foo}", __FILE__, __LINE__)
+    end
+  end
+
   class UpdateSiloWorker
     include Sidekiq::Worker
 
     def perform(item_id, item_class, name, mode="save", generator=nil, callback=nil)
-      @item_class, @generator, @callback = item_class, generator, callback
-      @item_id = item_id.kind_of?(String) ? item_id : item_id["$oid"]
+      @item_class = item_class.constantize
+      @generator  = generator ? generator.constantize : nil
+      @callback   = callback
+      @item_id    = item_id.kind_of?(String) ? item_id : item_id["$oid"]
+
       mode.to_s == "save" ? update_silo(name, generator) : destroy_silo(name)
     end
 
     private
 
     def update_silo name, generator
-      @item = item_class.send(:find, @item_id)
+      @item = @item_class.send(:find, @item_id)
       @silo = Silo.where(item_class: @item_class, item_id: @item_id, silo_type: name).first
-      @content = generator_class.send(:new, @item).generate
+      @content = @generator.send(:new, @item).generate
+
       if @silo
         @silo.set(:bag, @content)
       else
         @silo = Silo.create(item_class: @item_class, item_id: @item_id, bag: @content, silo_type: name)
       end
+
       unless @callback.nil? || @callback == ""
         @item.__send__(@callback, :updated)
       end
@@ -28,30 +39,14 @@ module MongoidSilo
 
     def destroy_silo name
       @silo = Silo.where(item_class: @item_class, item_id: @item_id, silo_type: name).first
+
       if @silo
         @silo.destroy
       end
+
       unless @callback.nil? || @callback == ""
         @item.__send__(@callback, :destroyed)
       end
-    end
-
-    def item_class
-      cl = nil
-      @item_class.split("::").inject(nil) do |parent, identifier|
-        parent ||= Kernel
-        cl = parent.const_get(identifier)
-      end
-      cl
-    end
-
-    def generator_class
-      cl = nil
-      @generator.split("::").inject(nil) do |parent, identifier|
-        parent ||= Kernel
-        cl = parent.const_get(identifier)
-      end
-      cl
     end
   end
 end
